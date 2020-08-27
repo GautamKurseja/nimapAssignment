7663f94349df5041cdcffcaa2a036d595ea322b3
GautamKurseja <40169032+GautamKurseja@users.noreply.github.com>
2020-08-28 03:34:22 +0530

    "first commit"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from "./home-page/home-page.component";
+import { UserProfileComponent } from "./user-profile/user-profile.component";
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomePageComponent, pathMatch: "full" },
+  { path: "home", component: HomePageComponent, pathMatch: "full" },
+  { path: "UserProfile/:id", component: UserProfileComponent },
+  { path: "**", component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
